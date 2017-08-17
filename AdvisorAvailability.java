9a9d7c95120a2b08836968d3de09e4b7e52eed45
pchandaka <pchandaka@users.noreply.github.com>
2017-08-17 15:35:34 -0500

    Add files via upload

---
+++ AdvisorAvailability.java
@@ -0,0 +1,230 @@
+package com.chandaka;
+
+public class AdvisorAvailability {
+	String advisor;
+	String month;
+	String dates;
+	String day;
+	String cst08;
+	String cst0830;
+	String cst09;
+	String cst0930;
+	String cst10;
+	String cst1030;
+	String cst11;
+	String cst1130;
+	String cst12;
+	String cst1230;
+	String cst13;
+	String cst1330;
+	String cst14;
+	String cst1430;
+	String cst15;
+	String cst1530;
+	String cst16;
+	String cst1630;
+	String cst17;
+	String cst1730;
+	String cst18;
+
+	public String getAdvisor() {
+		return advisor;
+	}
+
+	public void setAdvisor(String advisor) {
+		this.advisor = advisor;
+	}
+
+	public String getMonth() {
+		return month;
+	}
+
+	public void setMonth(String month) {
+		this.month = month;
+	}
+
+	public String getDates() {
+		return dates;
+	}
+
+	public void setDates(String dates) {
+		this.dates = dates;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getCst08() {
+		return cst08;
+	}
+
+	public void setCst08(String cst08) {
+		this.cst08 = cst08;
+	}
+
+	public String getCst0830() {
+		return cst0830;
+	}
+
+	public void setCst0830(String cst0830) {
+		this.cst0830 = cst0830;
+	}
+
+	public String getCst09() {
+		return cst09;
+	}
+
+	public void setCst09(String cst09) {
+		this.cst09 = cst09;
+	}
+
+	public String getCst0930() {
+		return cst0930;
+	}
+
+	public void setCst0930(String cst0930) {
+		this.cst0930 = cst0930;
+	}
+
+	public String getCst10() {
+		return cst10;
+	}
+
+	public void setCst10(String cst10) {
+		this.cst10 = cst10;
+	}
+
+	public String getCst1030() {
+		return cst1030;
+	}
+
+	public void setCst1030(String cst1030) {
+		this.cst1030 = cst1030;
+	}
+
+	public String getCst11() {
+		return cst11;
+	}
+
+	public void setCst11(String cst11) {
+		this.cst11 = cst11;
+	}
+
+	public String getCst1130() {
+		return cst1130;
+	}
+
+	public void setCst1130(String cst1130) {
+		this.cst1130 = cst1130;
+	}
+
+	public String getCst12() {
+		return cst12;
+	}
+
+	public void setCst12(String cst12) {
+		this.cst12 = cst12;
+	}
+
+	public String getCst1230() {
+		return cst1230;
+	}
+
+	public void setCst1230(String cst1230) {
+		this.cst1230 = cst1230;
+	}
+
+	public String getCst13() {
+		return cst13;
+	}
+
+	public void setCst13(String cst13) {
+		this.cst13 = cst13;
+	}
+
+	public String getCst1330() {
+		return cst1330;
+	}
+
+	public void setCst1330(String cst1330) {
+		this.cst1330 = cst1330;
+	}
+
+	public String getCst14() {
+		return cst14;
+	}
+
+	public void setCst14(String cst14) {
+		this.cst14 = cst14;
+	}
+
+	public String getCst1430() {
+		return cst1430;
+	}
+
+	public void setCst1430(String cst1430) {
+		this.cst1430 = cst1430;
+	}
+
+	public String getCst15() {
+		return cst15;
+	}
+
+	public void setCst15(String cst15) {
+		this.cst15 = cst15;
+	}
+
+	public String getCst1530() {
+		return cst1530;
+	}
+
+	public void setCst1530(String cst1530) {
+		this.cst1530 = cst1530;
+	}
+
+	public String getCst16() {
+		return cst16;
+	}
+
+	public void setCst16(String cst16) {
+		this.cst16 = cst16;
+	}
+
+	public String getCst1630() {
+		return cst1630;
+	}
+
+	public void setCst1630(String cst1630) {
+		this.cst1630 = cst1630;
+	}
+
+	public String getCst17() {
+		return cst17;
+	}
+
+	public void setCst17(String cst17) {
+		this.cst17 = cst17;
+	}
+
+	public String getCst1730() {
+		return cst1730;
+	}
+
+	public void setCst1730(String cst1730) {
+		this.cst1730 = cst1730;
+	}
+
+	public String getCst18() {
+		return cst18;
+	}
+
+	public void setCst18(String cst18) {
+		this.cst18 = cst18;
+	}
+
+}
